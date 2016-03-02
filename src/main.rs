14506abf13af7741599745c273483291a5d97c78
Andrew-pa <andrew.pa@outlook.com>
2016-03-01 18:20:00 -0800

    progress!

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,97 @@
-extern crate winapi;
-extern crate kernel32;
 
-use winapi::*;
-use kernel32::*;
+extern crate rustbox;
 
-trait Console {
-	fn move_cursor(&self, x : i16, y : i16);
+use std::error::Error;
+use std::default::Default;
+
+use rustbox::{Color, RustBox};
+use rustbox::Key;
+
+struct Buffer {
+    lines : Vec<String>,
+    cur_idx: usize,
+    cur_col: usize,
+    cur_row: usize,
 }
 
-struct Win32Console {
-	screen_buffer : HANDLE
-}
+impl Buffer {
+    fn new() -> Buffer {
+        Buffer { 
+            lines: vec![String::new()], 
+            cur_idx:0, 
+            cur_col:0,
+            cur_row:0,
+        }
+    }
 
-impl Win32Console {
-	fn new() -> Win32Console {
-		Win32Console {
-			screen_buffer : unsafe {
-				kernel32::CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, FILE_SHARE_WRITE,
-					0 as *const SECURITY_ATTRIBUTES, CONSOLE_TEXTMODE_BUFFER, 0 as LPVOID)
-			}
-		}
-	}
-}
+    fn insert(&mut self, c : char) {
+        let cr = self.cur_row;
+        let ci = self.cur_idx;
+        if ci == self.lines[cr].len() {
+            self.lines[cr].push(c);
+        } else {
+            self.lines[cr].insert(ci, c);
+        }
+        self.cur_col += 1;
+        self.cur_idx += c.len_utf8(); //Strings are UTF8
+    }
 
-impl Console for Win32Console {
-	fn move_cursor(&self, x : i16, y : i16) {
-		unsafe {
-			kernel32::SetConsoleCursorPosition(self.screen_buffer, COORD {X: x, Y: y});
-		}
-	}
-}
+    fn new_line(&mut self) {
+        self.lines.push(String::new());
+        self.cur_row += 1;
+        self.cur_idx = 0;
+        self.cur_col = 0;
+    }
 
-impl Drop for Win32Console {
-	fn drop(&mut self) {
-		unsafe {
-			kernel32::CloseHandle(self.screen_buffer);
-		}
-	}
+    fn draw(&self, x: usize, y: usize, rustbox: &RustBox) {
+        let mut cy = y;
+        for ln in &self.lines {
+            rustbox.print(x, cy, rustbox::RB_NORMAL, Color::White, Color::Black, &ln);
+            cy += 1;
+        }
+        rustbox.set_cursor(x+self.cur_col as isize, y+self.cur_row as isize);
+    }
 }
 
 fn main() {
-	let cnsl = Win32Console::new();
+    let rustbox = match RustBox::init(Default::default()) {
+        Result::Ok(v) => v,
+        Result::Err(e) => panic!("{}", e),
+    };
 
-	println!("hello, world!");
-	let mut x = 30i16;
-	loop { cnsl.move_cursor(x, 30); x += 1; if x > 80 {x = 30;} }
+    let mut buf = Buffer::new();
+    buf.new_line();
+    let mut buf_changed = false;
+    //rustbox.print(1, 3, rustbox::RB_BOLD, Color::White, Color::Black,
+    //             "Press 'q' to quit.");
+    rustbox.present();
+    loop {
+        match rustbox.poll_event(false) {
+            Ok(rustbox::Event::KeyEvent(key)) => {
+                match key {
+                    Key::Char(c) => { 
+                        buf.insert(c);
+                        buf_changed = true;
+                    }
+                    Key::Enter => {
+                        buf.new_line();
+                        buf_changed = true;
+                    }
+                    Key::Left => {
+                    }
+                    Key::Esc => { break; }
+                    _ => { }
+                }
+            },
+            Err(e) => panic!("{}", e.description()),
+            _ => { }
+        }
+        if buf_changed {
+            buf_changed = false;
+            rustbox.clear();
+            buf.draw(1,1,&rustbox);
+//rustbox.print(1, 1, rustbox::RB_NORMAL, Color::Green, Color::Black, &buf);
+            rustbox.present();
+        }
+    }
 }
