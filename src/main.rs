ca254ff69db3637596b65f7fe068d5ac840e8c82
andrew-pa <andrew.pa@outlook.com>
2017-04-05 17:28:11 -0700

    silly prototype editor. We need modal editing!

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,10 @@ impl Buffer {
         Buffer { lines: LinkedList::new() }
     }
 
-    fn draw(win: &Window) {
+    fn draw(&self, (x0,y0): (i32,i32), win: &Window) {
+        for (y,l) in (y0..).zip(self.lines.iter()) {
+            win.mvprintw(y, x0, l);
+        }
     }
 }
 
@@ -21,13 +24,37 @@ fn main() {
   window.refresh();
   window.keypad(true);
   noecho();
+
+  let mut cur_x = window.get_cur_x();
+  let mut cur_y = window.get_cur_y();
+
+ // let subwin = window.subwin(8,40,2,2).unwrap();
+  
+  //  subwin.border('|','|','-','-','+','+','+','+');
+    let mut buf = Buffer::new();
+    buf.lines.push_front(String::from("Hello, World! 1"));
+    buf.lines.push_front(String::from("Hello, World! 2"));
+    buf.lines.push_front(String::from("Hello, World! 3"));
+    buf.lines.push_front(String::from("Hello, World! 4"));
+  //  subwin.refresh();
+
   loop {
       match window.getch() {
-          Some(Input::Character(c)) => { window.addch(c); },
+          Some(Input::KeyUp) => { cur_y -= 1 },
+          Some(Input::KeyDown) => { cur_y += 1 },
+          Some(Input::KeyLeft) => { cur_x -= 1 },
+          Some(Input::KeyRight) => { cur_x += 1 },
+          Some(Input::Character(c)) => {
+              buf.lines.iter_mut().nth(cur_y as usize).unwrap().insert(cur_x as usize, c);
+              cur_x += 1
+          },
           Some(Input::KeyDC) => break,
           Some(input) => { window.addstr(&format!("{:?}", input)); },
           None => ()
       }
+      buf.draw((0,0), &window);
+      window.mv(cur_y, cur_x);
+      window.refresh();
   }
   endwin();
 }
