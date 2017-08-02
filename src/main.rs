1df5ad2273d00fc8741b6a7e963ca3fd6dc736a2
Andrew Palmer <andrew.pa@outlook.com>
2017-08-02 13:38:54 -0700

    scrolling viewport

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,8 @@ struct TxdApp {
     buf: Buffer,
     fnt: Font,
     line_layouts: Vec<Option<TextLayout>>,
+    viewport_start: usize,
+    viewport_end: usize,
     cursor_line: usize,
     cursor_col: usize
 }
@@ -22,9 +24,9 @@ impl TxdApp {
     fn init(mut rx: &mut RenderContext) -> TxdApp {
         let mut app = TxdApp {
             buf: Buffer::load(Path::new("src\\main.rs")).expect("open file"),
-            fnt: Font::new(&mut rx, String::from("Consolas"), 14.0, runic::FontWeight::Regular, runic::FontStyle::Normal).expect("font"),
+            fnt: Font::new(&mut rx, String::from("Consolas"), 16.0, runic::FontWeight::Regular, runic::FontStyle::Normal).expect("font"),
             line_layouts: Vec::new(),
-            cursor_line: 0, cursor_col: 1
+            viewport_start: 0, viewport_end: 100, cursor_line: 0, cursor_col: 1
         };
         let bnd = rx.bounds();
         for ref line in app.buf.lines.iter() {
@@ -32,16 +34,36 @@ impl TxdApp {
         }
         app
     }
+
+    fn move_cursor(&mut self, (dx, dy): (isize,isize)) {
+        let mut cursor_col = (self.cursor_col as isize + dx);
+        let mut cursor_line = (self.cursor_line as isize + dy);
+        if cursor_col < 0 { cursor_col = 0; }
+        if cursor_line < 0 { cursor_line = 0; }
+        if cursor_line >= self.buf.lines.len() as isize { cursor_line = (self.buf.lines.len()-1) as isize; }
+        let cln = self.buf.lines[cursor_line as usize].len();
+        if cursor_col as usize > cln { cursor_col = cln as isize; }
+
+        if cursor_line < self.viewport_start as isize {
+            self.viewport_start = self.viewport_start.saturating_sub(3);
+        }
+        if cursor_line >= self.viewport_end as isize {
+            self.viewport_start = self.viewport_start.saturating_add(3);
+        }
+
+        self.cursor_col = cursor_col as usize;
+        self.cursor_line = cursor_line as usize;
+    }
 }
 
 impl App for TxdApp {
     fn event(&mut self, e: Event) {
         match e {
             Event::Key(k, d) => match k {
-                KeyCode::Character('h') => { self.cursor_col -= 1; }
-                KeyCode::Character('j') => { self.cursor_line += 1; }
-                KeyCode::Character('k') => { self.cursor_line -= 1; }
-                KeyCode::Character('l') => { self.cursor_col += 1; }
+                KeyCode::Character('h') => { self.move_cursor((-1, 0)); }
+                KeyCode::Character('j') => { self.move_cursor((0, 1)); }
+                KeyCode::Character('k') => { self.move_cursor((0, -1)); }
+                KeyCode::Character('l') => { self.move_cursor((1, 0)); }
                 KeyCode::Character('x') => {
                     let (co,li) = (self.cursor_col, self.cursor_line);
                     self.buf.lines[li].remove(co);
@@ -57,8 +79,8 @@ impl App for TxdApp {
         rx.clear(Color::rgb(0.1, 0.1, 0.1));
         let mut p = Point::xy(4.0, 4.0);
         let bnd = rx.bounds();
-        let mut line = 0;
-        while p.y < bnd.h && line < self.line_layouts.len() {
+        let mut line = self.viewport_start;
+        while p.y < bnd.h-10.0 && line < self.line_layouts.len() {
             let mut replace = false;
             match self.line_layouts[line] {
                 Some(ref l) => { 
@@ -75,10 +97,13 @@ impl App for TxdApp {
             line += 1;
             }
         }
+        self.viewport_end = line;
+        //rx.draw_line(p, Point::xy(p.x+bnd.w, p.y), Color::rgba(0.2, 0.8, 0.1, 0.6), 3.0);
         let col = self.cursor_col;
         let mut cb = self.line_layouts[self.cursor_line].as_ref().unwrap().char_bounds(col);
         if cb.w == 0.0 { cb.w = 8.0; }
-        rx.fill_rect(cb.offset(Point::xy(4.0,4.0+cb.h*self.cursor_line as f32)), Color::rgb(0.8, 0.6, 0.0));
+        rx.fill_rect(cb.offset(Point::xy(4.0,4.0+cb.h*(self.cursor_line-self.viewport_start) as f32)),
+            Color::rgba(0.8, 0.6, 0.0, 0.9));
     }
 }
 
