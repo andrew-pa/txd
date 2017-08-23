d909c28a8513bb4726d1e91c954f1e76a78b309b
Andrew Palmer <andrew.pa@outlook.com>
2017-08-23 12:29:11 -0700

    new movements!

--- src/movement.rs
+++ src/movement.rs
@@ -3,17 +3,27 @@
 // w: forward one word
 // b: backward one word
 // e: forward one word, place at end
+// f[char]/F[char]: scan forward/backward for char, place cursor on it
+// t[char]/T[char]: scan forward/backward for char, place cursor before/after it
+// $: end of line
+// ^: start of line
 // <number>[mov]: repeated movement n times
 #[derive(Debug, Clone)]
 pub enum Movement {
     Char(bool),
     Line(bool),
-    Word(bool, bool),
+    Word(bool, bool), // (forward, place at end)
+    CharScan {
+        query: char, forwards: bool, place_besides: bool
+    },
+    StartOfLine,
+    WholeLine,
+    EndOfLine,
     Rep(usize, Box<Movement>)
 }
 
 impl Movement {
-    pub fn parse(s: &str) -> Option<Movement> {
+    pub fn parse(s: &str, first: bool) -> Option<Movement> {
         //println!("parse movment {}", s);
         use self::Movement::*;
         let mut cs = s.char_indices();
@@ -25,17 +35,30 @@ impl Movement {
                         if !c.is_digit(10) { end = j; break; }
                     }
                     let sp = s.split_at(end);
-                    sp.0.parse::<usize>().ok().and_then(|n| Movement::parse(sp.1).map(|m| Rep(n,Box::new(m)))) 
+                    sp.0.parse::<usize>().ok().and_then(|n| Movement::parse(sp.1,false).map(|m| Rep(n,Box::new(m)))) 
                 } else {
                     match c {
                         'h' => Some(Char(false)),
                         'j' => Some(Line(false)),
                         'k' => Some(Line(true)),
                         'l' => Some(Char(true)),
-                        'w' => Some(Word(false, false)),
-                        'b' => Some(Word(true, false)),
+                        'w' => Some(Word(true, false)),
+                        'b' => Some(Word(false, false)),
                         'e' => Some(Word(false, true)),
-                        _ => None
+                        '^' => Some(StartOfLine),
+                        'J' => Some(WholeLine),
+                        '$' => Some(EndOfLine),
+                        't' => cs.next().map(|(_,q)| CharScan { query: q, forwards: true, place_besides: true }),
+                        'T' => cs.next().map(|(_,q)| CharScan { query: q, forwards: false, place_besides: true }),
+                        'f' => cs.next().map(|(_,q)| CharScan { query: q, forwards: true, place_besides: false }),
+                        'F' => cs.next().map(|(_,q)| CharScan { query: q, forwards: false, place_besides: false }),
+                        _ => if !first {
+                            match c {
+                                'd' => Some(WholeLine),
+                                'c' => Some(WholeLine),
+                                _ => None
+                            }
+                        } else { None }
                     }
                 }
             },
